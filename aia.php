0b26ed0bab82db941ccb666b34b7ad10f1552c06
catclops <68121329+catclops@users.noreply.github.com>
2021-03-01 14:01:12 -0800

    AIA Assignment upload

--- aia.php
+++ aia.php
@@ -1,6 +1,97 @@
 <?php include "includes/header.php";?>
 
-<h2 class="subheader">Template</h2>
-<p>Use this as a model for creating new pages</p>
+<h2 class="subheader">Audience, Issues, and Approach</h2>
+<section>
+<table class="client">
+              <tr>
+                   <th>Client's name and final decision maker</th>
+                   <td>Toan Nguyen is the client and he is my brother.</td>
+              </tr>   
+              <tr>
+                   <th>Client's business/website name</th>
+                   <td>Trading Tone</td>
+              </tr>
+              <tr>
+                   <th>Redesign or brand new site</th>
+                   <td>A brand new website</td>
+              </tr>
+              <tr>
+                   <th>Type of website</th>
+                   <td>Marketing, Shopping, and Social Networking</td>
+              </tr>  
+              <tr>
+                   <th>Client's goal</th>
+                   <td>My client hopes to connect with sports trading card enthusiasts in order to discuss, buy, sell, and trade cards.</td>
+              </tr>
+         </table>
+         
+<table class="audience">
+              <tr>
+                   <th>Audience</th>
+                   <th>Issues</th>
+                   <th>Approach</th>
+              </tr>  
+              <tr>
+                   <td><strong>Potential Customers:</strong><br>
+                       they are likely competent in online browsing through the most commonly used browsers (Firefox, Chrome, or Safari). They are probably financially stable since they are sports trading card collectors and should be able to own a smart phone, a laptop, or a desktop. They will need to rely on pictures and image clarity so enough bandwidth for the website to host images is a neccessary.
+                   </td>
+                   <td>
+                   <ul>
+                     <li>Site reliability is a must.</li>
+                     <li>Pictures should be clear and descriptions for the card must be detailed.</li>
+                     <li>The want to be able to engage with the seller to find out more about the product.</li>
+                     <li>They want to know about shipping and returns.</li>
+                    </ul>
+                     </td>
+                   <td>
+                   <ul>
+                     <li>Build a reliable website.</li>
+                     <li>Make sure the hosting space for the website has plenty of storage space.</li>
+                     <li>Enable a messaging system.</li>
+                     <li>Include in the FAQ section the policies about shipping.</li>
+                    </ul>
+                     </td>
+              </tr>  
+              <tr>
+                  <td><strong>Current Customers:</strong><br> 
+                        they already have an established presence on the site and are reliant on image quality. They share several overlaps with potential customers in regards to the appeal of the website. We need to incorporate an advertising system for them to show off their collection since this is meant to be a trading platform.</td>
+                   <td>
+                   <ul>
+                      <li>They want to be able to remove out scammers and unscrupulous sellers.</li>
+                      <li>Pictures should be clear and descriptions for the card must be detailed.</li>
+                      <li>They want to interact with serious enthusiasts.</li>
+                    </ul></td>
+                   <td>
+                   <ul>
+                     <li>Make a reputation rating system tied to each individual account.</li>  
+                     <li>Make sure that server space isn’t an issue.</li>
+                     <li>Include a private messaging system or a community forum</li>   
+                       </ul>
+                
+                  </td>
+              </tr>  
+              <tr>
+                   <td><strong>Potential Vendors:</strong><br> 
+                       prospective wholesalers should already have their own established online presence so the layout of our site should clearly match their basic expectations. The users are likely to own desktop computers and have ample experience in online retail. </td>
+                   <td>
+                   <ul>
+                     <li>They have no history of business with my client.</li>
+                     <li>They are unclear about the reliability and professionalism of my client.</li>
+                     <li>They need to know how large wholesaler shipments are processed.</li>
+                     <li>They want to know the hours of operations.</li>
+                    </ul>
+                     </td>
+                   <td>
+                   <ul>
+                     <li>Include an “About Me” section in the website to demonstrate my client’s background.</li>
+                     <li>Create a rating system for profiles to track their reliability.</li>
+                     <li>Create an “FAQ” section with a subsection for large shipments.</li>
+                     <li>Include the business hours in the “Contact Us” section.</li>
+                    </ul>
+                     </td>
+              </tr>  
+         
+         </table>
+     </section>
  
 <?php include "includes/footer.php";?>
